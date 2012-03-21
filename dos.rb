7df0acec3cffe1ae2090290424fc61ca61e49b81
Michal Kwiatkowski <michal@trivas.pl>
2012-03-21 16:06:19 +0100

    Extract another method.

--- dos.rb
+++ dos.rb
@@ -11,13 +11,9 @@ end
 
 class Connections
   def initialize(tweets)
-    @authors_mentions = authors_mentions_from_tweets(tweets)
+    initialize_authors_mentions_from_tweets(tweets)
     @connections = tweets.map(&:author).inject(Hash.new([])) do |h, author|
-      conn = @authors_mentions[author].select { |other_person|
-        @authors_mentions.fetch(other_person, []).include?(author)
-      }.sort
-      h[author] = conn
-      h
+      h.merge(author => mutual_mentions(author))
     end
   end
 
@@ -26,10 +22,16 @@ class Connections
   end
 
   private
-  def authors_mentions_from_tweets(tweets)
-    tweets.inject({}) do |m, tweet|
+  def initialize_authors_mentions_from_tweets(tweets)
+    @authors_mentions = tweets.inject({}) do |m, tweet|
       m[tweet.author] = tweet.mentions
       m
     end
+  end
+
+  def mutual_mentions(person)
+    @authors_mentions[person].select { |other_person|
+      @authors_mentions.fetch(other_person, []).include?(person)
+    }.sort
   end
 end
