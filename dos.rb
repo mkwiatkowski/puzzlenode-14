141d76a4d35c0d417c248053b62ef40772354a31
Michal Kwiatkowski <michal@trivas.pl>
2012-03-21 16:07:52 +0100

    And another extract method.

--- dos.rb
+++ dos.rb
@@ -11,10 +11,8 @@ end
 
 class Connections
   def initialize(tweets)
-    initialize_authors_mentions_from_tweets(tweets)
-    @connections = tweets.map(&:author).inject(Hash.new([])) do |h, author|
-      h.merge(author => mutual_mentions(author))
-    end
+    initialize_authors_mentions(tweets)
+    initialize_first_order_connections(tweets)
   end
 
   def order(n, person)
@@ -22,10 +20,16 @@ class Connections
   end
 
   private
-  def initialize_authors_mentions_from_tweets(tweets)
+  def initialize_authors_mentions(tweets)
     @authors_mentions = tweets.inject({}) do |m, tweet|
       m[tweet.author] = tweet.mentions
       m
+    end
+  end
+
+  def initialize_first_order_connections(tweets)
+    @connections = tweets.map(&:author).inject(Hash.new([])) do |h, author|
+      h.merge(author => mutual_mentions(author))
     end
   end
 
